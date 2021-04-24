acad00ae368084b85e700443543f257c08de5dd5
Ricardo Martins <ricardofsmartins@gmail.com>
2021-04-24 19:09:44 +0100

    Bugged fixed

--- sketch.js
+++ sketch.js
@@ -64,6 +64,7 @@ function draw()
     // Print trial count at the top left-corner of the canvas
     fill(color(255,255,255));
     textAlign(LEFT);
+    textSize(18);
     text("Trial " + (current_trial + 1) + " of " + trials.length, 50, 20);
 
     // Print hit combo at center of screen, above the grid
